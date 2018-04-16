0058b105126329f18b89427d652bc37b18d29830
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-04-15 20:11:08 -0700

    sort out path to defaultconstants

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,12 @@
+import path from 'path'
 let config
-
+const defaultConstPath = path.join(process.env.REACT_WEBPACK_ROOT, 'cfg', 'defaultConstants')
 if (typeof constants !== 'undefined') {
   // The global 'constants' is being defined by Webpack in /cfg/[dev|dist|test|localhost].js
   // eslint-disable-next-line no-undef
   config = constants
 } else {
-  const defaultConstants = require('../../cfg/defaultConstants')
+  const defaultConstants = require(defaultConstPath)
   config = defaultConstants.default || defaultConstants
 }
 
