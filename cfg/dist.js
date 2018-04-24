ab80564e02759378e0f04e51487e60cf3fa3058d
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-04-23 17:42:41 -0700

    resolve aliases

--- cfg/dist.js
+++ cfg/dist.js
@@ -16,23 +16,23 @@ const config = Object.assign({}, baseConfig, {
       siaRoot,
       path.resolve(siaRoot, 'src'),
       path.resolve(siaRoot, 'node_modules')
-    ]
-  //   alias: {
-  //     cfg: path.resolve(siaRoot, 'cfg'),
-  //     src: path.resolve(siaRoot, 'src'),
-  //     actions: path.resolve(siaRoot, 'src', 'actions'),
-  //     components: path.resolve(siaRoot, 'src', 'components'),
-  //     config: path.resolve(siaRoot, 'src', 'config'),
-  //     containers: path.resolve('src', 'containers'),
-  //     extensionHooks: path.resolve('src', 'extensionHooks'),
-  //     helpers: path.resolve(siaRoot, 'src', 'helpers'),
-  //     reducers: path.resolve(siaRoot, 'src', 'reducers'),
-  //     services: path.resolve(siaRoot, 'src', 'services'),
-  //     static: path.resolve(siaRoot, 'src', 'static'),
-  //     styles: path.resolve(siaRoot, 'src', 'styles'),
-  //     configureStore: path.resolve(siaRoot, 'src', 'configureStore'),
-  //     appInsights: path.resolve(siaRoot, 'src', 'appInsights.js')
-  //   }
+    ],
+    alias: {
+      cfg: path.resolve(siaRoot, 'cfg'),
+      src: path.resolve(siaRoot, 'src'),
+      actions: path.resolve(siaRoot, 'src', 'actions'),
+      components: path.resolve(siaRoot, 'src', 'components'),
+      config: path.resolve(siaRoot, 'src', 'config'),
+      containers: path.resolve('src', 'containers'),
+      extensionHooks: path.resolve('src', 'extensionHooks'),
+      helpers: path.resolve(siaRoot, 'src', 'helpers'),
+      reducers: path.resolve(siaRoot, 'src', 'reducers'),
+      services: path.resolve(siaRoot, 'src', 'services'),
+      static: path.resolve(siaRoot, 'src', 'static'),
+      styles: path.resolve(siaRoot, 'src', 'styles'),
+      configureStore: path.resolve(siaRoot, 'src', 'configureStore'),
+      appInsights: path.resolve(siaRoot, 'src', 'appInsights.js')
+    }
   },
   resolveLoader: {
     modules: [
